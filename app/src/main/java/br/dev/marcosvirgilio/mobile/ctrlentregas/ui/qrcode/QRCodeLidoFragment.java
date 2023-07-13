b736bf0edb958317e7334f8f312f3d61e36f3691
Marcos Virgilio <marcosvirgilio@gmail.com>
2023-07-13 13:54:16 -0300

    2023-07-13 - Versão Beta 1 - OK

--- app/src/main/java/br/dev/marcosvirgilio/mobile/ctrlentregas/ui/qrcode/QRCodeLidoFragment.java
+++ app/src/main/java/br/dev/marcosvirgilio/mobile/ctrlentregas/ui/qrcode/QRCodeLidoFragment.java
@@ -3,6 +3,7 @@ package br.dev.marcosvirgilio.mobile.ctrlentregas.ui.qrcode;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,12 +15,13 @@ import br.dev.marcosvirgilio.mobile.ctrlentregas.QRCodeSingleton;
 import br.dev.marcosvirgilio.mobile.ctrlentregas.R;
 
 
-public class QRCodeLidoFragment extends Fragment {
+public class QRCodeLidoFragment extends Fragment implements View.OnClickListener {
 
     private EditText etCd;
     private EditText etNm;
     private Button btConfirmar;
     private Button btCancelar;
+    private NavController navController;
     View view;
 
 
@@ -27,6 +29,7 @@ public class QRCodeLidoFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.navController  = QRCodeSingleton.getInstance().getNavController();
 
     }
 
@@ -37,10 +40,27 @@ public class QRCodeLidoFragment extends Fragment {
         this.view = inflater.inflate(R.layout.fragment_qr_code_lido, container, false);
 
         this.etCd = view.findViewById(R.id.etCodigo);
+        this.btCancelar = view.findViewById(R.id.btCancelar);
+        this.btConfirmar = view.findViewById(R.id.btConfirmar);
+
+        this.btCancelar.setOnClickListener(this);
+        this.btConfirmar.setOnClickListener(this);
         //mostrando valor do singleton
         QRCodeSingleton singleton = QRCodeSingleton.getInstance();
         this.etCd.setText(singleton.getQrCode());
 
         return this.view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btConfirmar) {
+            //chamando navegação
+            navController.navigate(R.id.navigation_qrcode);
+        }
+        if (view.getId() == R.id.btCancelar) {
+            //chamando navegação
+            navController.navigate(R.id.navigation_qrcode);
+        }
     }
 }
